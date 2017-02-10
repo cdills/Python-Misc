94e41ab4e2a3872f681f90682c8592992b2a939e
Cody Dills <dillscody@gmail.com>
2017-02-10 11:51:45 -0500

    Add files via upload

--- rlExchange_searchengine.py
+++ rlExchange_searchengine.py
@@ -1,33 +1,69 @@
+# Searches rocketleagueexchange for desired items, outputs to and opens html page.
+
 from __future__ import print_function
 import praw
 import webbrowser
-outputFile=open("Python/results.htm", 'w', errors='ignore')
 
-reddit = praw.Reddit(client_id='', client_secret="",
-                     password='', user_agent='Python:RL_Exchange_Search',
-                     username='')
 
-subreddit = reddit.subreddit('RocketLeagueExchange')
-
-uQuery=input("Enter desired item. e.g. Hypernova:  ")
-uLimit=input("How many results? e.g. 10:  ")
-uSort=input("New, Top, or Hot:  ")
-uTime=input("Seach post from the last hour, day, week, month, or year?:  ")
-uTitle="Searching for: " + uSort + " post(s) containing " + uQuery + " posted in the last " + uTime + "."
-
-sTags='''<html>
-<head>'''+ uTitle.upper() +'''</head>
-<body><p>'''
-
-eTags='''</p></body>
-</html>'''
-
-print (sTags, file=outputFile)
-
-for submission in subreddit.search(uQuery, sort=uSort, limit=int(uLimit), time_filter=uTime.lower()):
-
+def reddit_auth():
+	global reddit, subreddit
+	reddit = praw.Reddit(client_id="", client_secret="",
+	password='', user_agent='',
+	username='')
+	subreddit = reddit.subreddit('RocketLeagueExchange')
+def get_input():
+	global uFlair, fQuery, uLimit, uSort, uTime, uTitle
+	gFlair=input("Enter PC, PS4, or Xbox: ") or "PC"
+	uFlair="[" + gFlair + "]"
+	uQuery=input("Enter desired item. e.g. Hypernova:  ") or "Photon"
+	fQuery=(uFlair + " " + uQuery)
+	uLimit=input("How many results? e.g. 10:  ") or 10
+	uSort=input("Sort by relevance, hot, top, new, or comments?:  ") or "relevance"
+	uTime=input("Seach post from the last hour, day, week, month, or year?:  ") or "hour"
+	uTitle="Searching for post(s) containing: \"" + fQuery + "\" posted in the last " + uTime + " sorted by " + uSort
+def create_htm():
+	global	filepath, outputFile
+	filepath="/Scripts/Python/out.html"
+	outputFile=open(filepath, 'w', errors='ignore')
+	sTags='''<html>
+	<head>'''+ uTitle.upper() +'''</head>
+	<body><p>'''
+	print (sTags, file=outputFile)
+def close_htm():
+	eTags='''</p></body>
+	</html>'''
+	print (eTags, file=outputFile)
+def add_urllist():
 	print("<a href=" + submission.url + ">" + submission.title + "</a>", sep='', end='<br>', file=outputFile)
+def do_search():
+	for submission in subreddit.search(fQuery, sort=uSort, limit=int(uLimit), time_filter=uTime.lower()):
+		print("<a href=" + submission.url + ">" + submission.title + "</a>", sep='', end='<br>', file=outputFile)
+def open_htm():
+	webbrowser.open(filepath)
+def email_self(fFile): # Doesn't work, email is blank
+	import	smtplib
+	from email.mime.text import MIMEText
+	gmail_user=""
+	gmail_pwd=""
+	fp = open(fFile, 'r')
+	msg = MIMEText(fp.read(), _subtype="html")
+	fp.close()
 	
-print (eTags, file=outputFile)
-
-webbrowser.open(outputFile)
+	msg['Subject'] = "RLE Trades"
+	msg['From'] = ""
+	msg['TO'] = ""
+	
+	s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+	s.ehlo()
+	s.starttls
+	s.login(gmail_user, gmail_pwd)
+	s.sendmail("to", "from", msg.as_string())
+	s.close
+	
+reddit_auth()
+get_input()
+create_htm()
+do_search()
+close_htm()
+# email_self(filepath)
+open_htm()
